be47955bb068bfad7836ab71bce7a49db6042948
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-12-21 22:44:11 -0300

    Validate to user an relation to user_acount

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  
+  validates :firtname, presence: true
+  validates :lastname, presence: true
+  validates :username, presence:true, uniqueness: true
+  validates :email, presence: true
+
+  has_many :user_acount
+  has_many :acounts, through: :user_acount
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
